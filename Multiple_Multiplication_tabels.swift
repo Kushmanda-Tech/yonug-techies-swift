c8c9347168dd88e2e73f3d0359a0fcc04b1003ee
kushmandatech <59148749+kushmandatech@users.noreply.github.com>
2021-06-27 13:06:40 -0700

    Create Multiple_Multiplication_tabels.swift

---
+++ Multiple_Multiplication_tabels.swift
@@ -0,0 +1,15 @@
+ var fromNumber = 21
+ var  toNumber = 30
+ 
+ var NumberForTable = fromNumber
+ 
+ for NumberForTable in (fromNumber...toNumber){
+    
+ 
+ for counter in (1...10)
+ {
+    print(" the answer of \(NumberForTable) Multiplied by \(counter) is \(counter*NumberForTable)")
+    counter == counter+1
+ };
+    NumberForTable == NumberForTable+1
+ };
